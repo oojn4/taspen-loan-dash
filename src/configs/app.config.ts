0051ef134b589e5b066dc929c25386e9f17a185e
oojn4 <fauzanfaldy4@gmail.com>
2024-09-27 20:47:51 +0700

    add base url

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -12,7 +12,7 @@ export type AppConfig = {
 const appConfig: AppConfig = {
   appName: '',
   locale: 'en',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://taspen-loan-dash.vercel.app',
   authSecret: 'IAADLu2Qu+xC4kteZfSUNOi5s/M6zSi7Z6tP85h4GBY=',
   authJwtAge: 1209600,
   backendApiUrl: 'https://overall-ros-statisticsindonesia-d818bd85.koyeb.app/',
